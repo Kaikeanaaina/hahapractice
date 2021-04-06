a3b2065805ae70275b35ef34bc60727bfe01b036
Kainoa Keanaaina <kanakahacks@Kainoas-MacBook-Pro.local>
2021-04-05 21:24:14 -1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      gender: null,
+      age: null,
+      bodyPart: ["head", "neck", "back", "shoulder", "hand", "arm", "forearm", "wrist", "chest", "hip", "quad", "calve", "foot", "toe"],
+      muscleJoint: null,
+    }
+  }
+  componentDidMount(){
+    this.setState(
+      {
+        gender: Math.floor(Math.random() * 10) < 4 ? "Male" : "Female",
+        age: Math.floor(Math.random() * 100)
+      }
+    )
+  }
+  randomBodyPart(bodyPart){
+    return bodyPart[Math.floor(Math.random()*bodyPart.length)]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hāhā Practice</h1>
+        <p>Gender: {this.state.gender}</p>
+        <p>Age: {this.state.age}</p>
+        <p>body part: {this.randomBodyPart(this.state.bodyPart)}</p>
+        <p>muscle or joint</p>
+        <p></p>
+        <p></p>
+      </div>
+    );
+  }
 }
 
-export default App;
+
